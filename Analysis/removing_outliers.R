e4a5aef713eddc156d9453859655a594f9d1e394
Low Hon Zheng <76779525+lowhonzheng@users.noreply.github.com>
2022-08-14 21:19:18 +0800

    Update removing_outliers.R

--- Analysis/removing_outliers.R
+++ Analysis/removing_outliers.R
@@ -6,13 +6,6 @@ str(data)
 #x_bmi5 is considered continuous feature
 #sleptim1, poorhlth, drnkwk1
 
-#menthlth, physhlth, sleeptim1, poorhlth, drnkwk1m, x_bmi5
-
-# x <- boxplot(data$BMI, ylab = "BMI")
-# title("Boxplot of BMI")
-# length(x$out) #x$out refers to the the values of the outliers
-# x_indices <- which(data$BMI %in% x$out) #this refers to the indices
-
 y <- boxplot(data$NUM_DRINKSPERWK, ylab = "Alcoholic drinks per week")
 title("Boxplot of Alcoholic drinks per week")
 length(unique(y$out))
